fbfe2c5a2f649670167fca85de1ab40cef7a6ce8
Kier Borromeo <seraphipod@gmail.com>
2018-07-28 03:25:46 +0800

    Config update

---
+++ src/config.js
@@ -0,0 +1,11 @@
+export default {
+  title: 'Sinepogi',
+  description: `Fresh-looking viewer for Resorts World Manila's online cinema, designed to make browsing more convenient.`,
+  email: 'seraphipod@gmail.com',
+  keywords: [
+    'resorts world manila',
+    'movies',
+    'blockbusterseats',
+    'cinema'
+  ]
+}
