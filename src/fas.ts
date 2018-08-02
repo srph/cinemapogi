57fbb74a2eaec280d1d484862a2c781cb86b59a9
Kier Borromeo <seraphipod@gmail.com>
2018-08-02 09:27:34 +0800

    Remove log

--- src/fas.ts
+++ src/fas.ts
@@ -1,5 +1,5 @@
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { fas } from '@fortawesome/free-solid-svg-icons'
-console.log(fas)
+
 // @TODO: Add only icons we use
 library.add(fas)
